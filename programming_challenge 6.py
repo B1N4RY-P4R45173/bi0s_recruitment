899dcd28a4ae1c753d5e96fe6a6441f1c0b39acc
koppakaajay <77494118+koppakaajay@users.noreply.github.com>
2023-11-18 18:12:19 +0530

    Update programming_challenge 6.py

--- programming_challenge 6.py
+++ programming_challenge 6.py
@@ -6,8 +6,7 @@ differences = 0
 for i in range(len(X)):
     if X[i] != lst[i]:
         differences += 1
-
-#print(f"There are {differences} differences between l1 and l2.")
+        
 if 2*Y >= differences:
     print ("YES")
 else:
